c13f0046d712844354677db3fac74492f80c0c54
sepyHR80 <heidarisepehr80@gmail.com>
2022-10-24 22:43:44 +0330

    rfterretvet
    rev

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,15 +119,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
-
+import os
 STATICFILES_DIRS = [
-    BASE_DIR / "statics",
+    os.path.join(BASE_DIR, 'static')
 ]
+STATIC_URL = '/static/'
 
+# for server
+# MEDIA_ROOT = '/home2/shopeton/public_html/media'
+
+# for local
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
 from django.conf.urls.static import static
 
 urlpatterns = [
@@ -23,5 +24,5 @@ urlpatterns = [
     path('',include('website.urls'))
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
