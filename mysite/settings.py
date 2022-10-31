caa04be27bdaa6492ccbf41acd48c6929e7d6c1f
sepyHR80 <heidarisepehr80@gmail.com>
2022-10-31 17:41:50 +0330

    last commit on windows
    aaa

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'website',
-    'blog'
+    'website.apps.WebsiteConfig',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
--- website/urls.py
+++ website/urls.py
@@ -2,9 +2,11 @@
 from django.urls import path
 from website.views import *
 
+app_name = 'website'
+
 urlpatterns = [
-    path('',index_home),
-    path('contact',contact_view),
-    path('about',about_view)
+    path('',index_home,name='index'),
+    path('contact',contact_view,name='contact'),
+    path('about',about_view,name='about')
 
 ]
